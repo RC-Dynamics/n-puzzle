f8bdf572d4ed00605b39d71e274f3ca8efa13d5e
Roberto <betocostaf@gmail.com>
2020-06-30 10:39:51 -0300

    Drawing the puzzle and numbers

--- src/view/puzzle.js
+++ src/view/puzzle.js
@@ -1,10 +1,19 @@
 function draw_puzzle() {
-  strokeWeight(2);
-  stroke(0);
   var square_size = canvas_size / p_size;
   for (var i = 0; i < p_config.length; i++) {
     for (var j = 0; j < p_config[i].length; j++) {
+      strokeWeight(2);
+      stroke(0);
       rect(square_size * i, square_size * j, square_size, square_size);
+      if (p_config[i][j] == 0) continue;
+      noStroke();
+      textSize(square_size / 3);
+      textAlign(CENTER, CENTER);
+      text(
+        p_config[i][j],
+        square_size * i + square_size / 2,
+        square_size * j + square_size / 2
+      );
     }
   }
 }
