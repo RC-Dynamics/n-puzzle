e320d40ad74575d98c46a31cdafb7a0f18fb1978
Roberto <betocostaf@gmail.com>
2020-06-30 10:12:37 -0300

    Creating initial state

---
+++ src/config.js
@@ -0,0 +1,25 @@
+var p_size = 3;
+var p_config = [];
+
+function create_random_puzzle() {
+  var tmp,
+    array = [],
+    current,
+    top = p_size * p_size;
+  for (var i = 0; i < top; ++i) array[i] = i;
+  if (top)
+    while (--top) {
+      current = Math.floor(Math.random() * (top + 1));
+      tmp = array[current];
+      array[current] = array[top];
+      array[top] = tmp;
+    }
+  var i,
+    j,
+    temparray = [],
+    chunk = p_size;
+  for (i = 0, j = array.length; i < j; i += chunk) {
+    temparray.push(array.slice(i, i + chunk));
+  }
+  return temparray;
+}
--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,8 @@
 function setup() {
   createCanvas(300, 300);
   background(255, 0, 0);
+  p_config = create_random_puzzle();
+  console.log(p_config);
 }
 
 function draw() {}
